a541042106a2a8eecf011dbb32c783ec52cf24a9
bijaypokhrel05 <077bei011@ioepc.edu.np>
2025-07-11 21:56:47 +0545

    solved all the higher-order-functions-i questions

--- workshops/10-higher-order-functions-i/03-whos-a-special/whos-a-special.js
+++ workshops/10-higher-order-functions-i/03-whos-a-special/whos-a-special.js
@@ -1,1 +1,39 @@
+/*
+Write a function whosASpecial that takes an array of pets and returns a string
+confirming what you already knew: all of the pets are very special!
+Feel free to add your own special pets to the array!
+
+Use .forEach in your answer.
+
+
+```javascript
+let specialPets = [{
+    name: 'Sadie',
+    species: 'cat'
+  }, {
+    name: 'Layla',
+    species: 'cat'
+  }, {
+    name: 'Bogie',
+    species: 'dog'
+  }
+];
+
+whosASpecial(specialPets);
+// => Sadie the cat is very special! Layla the cat is very special! Bogie the dog is very special!
+```
+*/
+
 // YOUR CODE BELOW
+function whosASpecial(arr) {
+    let newStr = '';
+    let n = 0;
+    arr.forEach((el)=> {
+        newStr += `${el.name} the ${el.species} is very special!`
+        if (n !== arr.length - 1) {
+            newStr += ' ';
+        }
+        n++;
+    })
+    return newStr;
+}
--- workshops/10-higher-order-functions-i/04-my-for-each/my-for-each.js
+++ workshops/10-higher-order-functions-i/04-my-for-each/my-for-each.js
@@ -1,1 +1,26 @@
+/*
+Write a function myForEach that accepts an array and a callback function. The
+behavior of myForEach should mirror the functionality of the native .forEach()
+array method as closely as possible
+
+```javascript
+let sum = 0;
+
+function addToSum(num) {
+    sum += num;
+}
+
+let nums = [1, 2, 3];
+
+myForEach(nums, addToSum);
+
+console.log(sum); // 6
+```
+*/
+
 // YOUR CODE BELOW
+function myForEach(arr, func) {
+    for (let i = 0; i < arr.length; i++) {
+        func(arr[i], i)
+    }
+}
